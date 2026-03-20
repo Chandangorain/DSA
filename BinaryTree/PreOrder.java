5eebc7bb78eda8958035c13e49a4a3e915f316d1
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-21 00:32:30 +0530

    PreOrder

---
+++ BinaryTree/PreOrder.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class PreOrder{
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    static class BinaryTree{
+            static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if (idx >= nodes.length || nodes[idx] == -1) {
+        return null;
+    }
+
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+
+
+        }
+    }
+
+    public static void main(String[]args){
+        int nodes[]={1,2,4,-2,0,6,5,4,2,-4};
+        BinaryTree tree= new BinaryTree();
+        Node root=tree.buildTree(nodes);
+        System.out.println(root.data);
+
+    }
+}
