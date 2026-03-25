862418f2b48e5bd5b0a1763fed972d7ba317a13c
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-26 01:10:29 +0530

    pyq

---
+++ Pyq.java
@@ -0,0 +1,72 @@
+/* 
+You are given a function.
+int CheckPassword(char str[], int n);
+The function accepts string str of size n as an argument. Implement the function which returns 1 if given string str is valid password else 0.
+str is a valid password if it satisfies the below conditions.
+
+– At least 4 characters
+– At least one numeric digit
+– At Least one Capital Letter
+– Must not have space or slash (/)
+– Starting character must not be a number
+Assumption:
+Input string will not be empty.
+
+Example:
+
+Input 1:
+aA1_67
+Input 2:
+a987 abC012
+
+Output 1:
+1
+Output 2:
+0
+
+*/
+
+import java.util.*;
+public class Pyq {
+    public static int matchpass(String s,int n){
+
+        if(n<4){        //At least 4 characters
+            return 0;
+        }
+
+        if(s.charAt(0)>='0' && s.charAt(0)<='9'){   //Starting character must not be a number
+            return 0;
+        }
+
+        int cap=0;
+        int num=0;
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){ // at least 1 capital
+                cap++;
+            }
+            if(s.charAt (i) == ' '|| s.charAt(i)=='/'){     // not extra space and /
+               return 0;
+            }
+
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){   //At least one numeric digit
+                num++;
+            }
+        }
+        if(cap>0  && num>0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        for(int i=0;i<s.length();i++){
+    
+        System.out.println(matchpass(s,s.length()));
+    }
+}
+    
+}
