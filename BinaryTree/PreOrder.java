18839444aff6eb6190faaacddde1eae151059e0e
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-03-26 20:33:38 +0530

    preorder traversal

--- BinaryTree/PreOrder.java
+++ BinaryTree/PreOrder.java
@@ -14,23 +14,39 @@ public class PreOrder{
 
     static class BinaryTree{
             static int idx = -1;
-        public static Node buildTree(int nodes[]){
+        public static Node buildTree(int nodes[]){      // node creation function
             idx++;
-            if (idx >= nodes.length || nodes[idx] == -1) {
-        return null;
+            if (nodes[idx]== -1) {  //-1= no child
+                return null;
     }
             Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
+
+            newNode.left = buildTree(nodes);    //recursive call of node creation
+            newNode.right = buildTree(nodes);   // recursive call
             return newNode;
+        }
+
+        // only for traversal
+        public static void preorder(Node root){
+            if(root==null){
+                return ;
+            }
+            System.out.println(root.data+" ");  // preint root->left->right
+            preorder(root.left);    // if root= 1, then 1.left .
+            preorder(root.right);
         }
     }
 
     public static void main(String[]args){
-        int nodes[]={1,2,4,-2,0,6,5,4,2,-4};
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree= new BinaryTree();
         Node root=tree.buildTree(nodes);
-        System.out.println(root.data);
+        // System.out.println(root.data);   this is for print root data
+
+        tree.preorder(root);  // preorder traversal : 1 2 4 5 3 6
+
+        
 
     }
+    
 }
