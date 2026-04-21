99d1f879127aaf940431b9e40c04daff40a7150d
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-04-22 01:26:58 +0530

    leetcode735

---
+++ leetcode/leet1358.java
@@ -0,0 +1,21 @@
+/* 
+Input: s = "aaacb"
+Output: 3
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+*/
+/*
+ pseudocode of this problem is given below in terms of code 
+ 
+
+*/
+import java.util.*;  
+
+
+
+public class leet1358 {
+
+    public static int numberofsubstring(String s){
+
+
+    }
+}
---
+++ leetcode/leet735.java
@@ -0,0 +1,68 @@
+/*
+leetcode 735 : 
+input : [5,10,-5]
+output : [5,10]
+explanation : The 10 and -5 collide resulting in 10.  The 5 and 10 never collide.
+*/
+
+
+
+import java.util.*;
+class Solution{
+public static int[]asteroidscollision(int[]asteroids) {
+	ArrayList<Integer>list=new ArrayList<>();
+	
+	for(int i=0;i<asteroids.length;i++){
+		list.add(asteroids[i]);
+		
+		}
+	Boolean changed =true;
+    while(changed){
+	changed=false;
+	for(int i=0;i<list.size()-1;i++){
+		int first=list.get(i);
+		int second=list.get(i+1);
+		if(first>0 && second<0){
+			if(Math.abs(first)<Math.abs(second)){
+			    list.remove(i);
+			    changed=true;
+			    break;
+			}
+			else if(Math.abs(first)>Math.abs(second)){
+			   list.remove(i+1);
+			   changed=true;
+			    break;
+			}
+			else{
+			   list.remove(i);
+			   list.remove(i+1);
+			   changed=true;
+			    break;
+			}
+		}
+
+	}
+	
+  }
+
+int []result=new int[list.size()];
+for(int i=0;i<list.size();i++){
+result[i]=list.get(i);
+}
+return result;
+ }
+
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int[]nums=new int[n];
+for(int i=0;i<n;i++){
+nums[i]=sc.nextInt();
+}
+int[] result=asteroidscollision(nums);
+for(int i = 0; i < result.length; i++){
+        System.out.print(result[i] + " ");
+    }
+}
+
+}
