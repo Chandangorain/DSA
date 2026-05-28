df43bf20dfd146fa6a5666f8a19c4aa2aa0fadcb
Chandan Gorain <chandangorain.cse2023@nsec.ac.in>
2026-05-28 20:59:29 +0530

    s

--- TCS/Sortedduplicate.java
+++ TCS/Sortedduplicate.java
@@ -1,3 +1,13 @@
+/*
+
+remove duplicate from sorted arr 
+input: [1,1,2]
+output: 2
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively. It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+
+
 
 import java.util.*;
 public class Sortedduplicate {
